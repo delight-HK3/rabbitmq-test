f1a8d2f6e61f273e6b8aee2e7ed8ae4990a71e85
leebean <dabean@storelabs.co.kr>
2025-11-11 18:48:21 +0900

    [Fix] 필요없는 Builder 삭제

--- src/main/java/com/example/rabbitmqtest/dto/MessageDTO.java
+++ src/main/java/com/example/rabbitmqtest/dto/MessageDTO.java
@@ -6,17 +6,7 @@ import lombok.*;
  * 메세지 전송하는 DTO
  */
 @Getter
-@ToString
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MessageDTO {
-
     private String title;
     private String content;
-
-    @Builder
-    public MessageDTO(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
 }
