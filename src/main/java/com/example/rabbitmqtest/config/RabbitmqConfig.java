3e04c713680b354fed9c1abf9f9b29023f857ee6
leebean <dabin310153@naver.com>
2025-12-21 13:33:28 +0900

    [Fix] 필요없는 import 삭제

--- src/main/java/com/example/rabbitmqtest/config/RabbitmqConfig.java
+++ src/main/java/com/example/rabbitmqtest/config/RabbitmqConfig.java
@@ -1,17 +1,8 @@
 package com.example.rabbitmqtest.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
