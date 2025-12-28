ce71b1a6c1b6e18a8a6995f80c9b5704f5b2545c
leebean <dabin310153@naver.com>
2025-12-28 15:50:44 +0900

    [Fix] DTO 객체 수정

--- src/main/java/com/example/rabbitmqtest/dto/MessageDTO.java
+++ src/main/java/com/example/rabbitmqtest/dto/MessageDTO.java
@@ -6,7 +6,16 @@ import lombok.*;
  * 메세지 전송하는 DTO
  */
 @Getter
+@ToString
 public class MessageDTO {
+
     private String title;
     private String content;
+
+    @Builder
+    public MessageDTO(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
 }
