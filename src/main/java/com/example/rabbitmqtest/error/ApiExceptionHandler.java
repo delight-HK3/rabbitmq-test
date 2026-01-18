bd82332f1346bb0f5acbad12181b1c19a5751274
delight-HK3 <dabin310153@naver.com>
2026-01-18 15:56:39 +0900

    [Add] connection Error 경우 handler 추가

--- src/main/java/com/example/rabbitmqtest/error/ApiExceptionHandler.java
+++ src/main/java/com/example/rabbitmqtest/error/ApiExceptionHandler.java
@@ -3,8 +3,13 @@ package com.example.rabbitmqtest.error;
 import com.example.rabbitmqtest.common.codes.ExceptionCode;
 import com.example.rabbitmqtest.common.response.ApiResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.AmqpConnectException;
+import org.springframework.amqp.AmqpIOException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.listener.api.RabbitListenerErrorHandler;
+import org.springframework.amqp.rabbit.support.ListenerExecutionFailedException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,7 +19,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  */
 @Slf4j
 @RestControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler implements RabbitListenerErrorHandler {
 
     /**
      * Json 정렬 Exception Handler
@@ -39,6 +44,27 @@ public class ApiExceptionHandler {
         log.error("AMQP connect error : {}", ex.getMessage(), ex);
         return ApiResponse.fail(ExceptionCode.RABBITMQ_CONNECTION_FAILED.getMessage()
                                 , ExceptionCode.RABBITMQ_CONNECTION_FAILED.getCode());
+    }
+
+    /**
+     * RabbitMQ 커넥션에 오류가 나는 경우 동작하는 handler
+     *
+     * @param amqpMessage the raw message received.
+     * @param channel AMQP channel for manual acks.
+     * @param message the converted spring-messaging message (if available).
+     * @param exception the exception the listener threw, wrapped in a
+     * {@link ListenerExecutionFailedException}.
+     * @return 예외를 다시 던져서 재시도
+     */
+    @Override
+    public Object handleError(Message amqpMessage, Channel channel, org.springframework.messaging.Message<?> message
+                            , ListenerExecutionFailedException exception) throws Exception {
+
+        if(exception.getCause() instanceof AmqpIOException) {
+            log.error("AMQP connect error : {}", exception.getMessage(), exception);
+        }
+
+        return exception;
     }
 
 }
